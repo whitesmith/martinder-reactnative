8c1eb89a16fd7b1a230735d530d5545dce83f9f5
Ricardo Pereira <m@ricardopereira.eu>
2017-01-26 13:06:03 +0000

    [common] Styling in a separate file

--- src/setup.js
+++ src/setup.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import {
   AppRegistry,
-  StyleSheet,
   Text,
   View,
   Button,
   Alert
 } from 'react-native';
+
+import styles from './styles';
 
 const onPressEvents = () => {
   Alert.alert('Events button pressed!');
@@ -39,24 +40,5 @@ function setup() {
 
   return Root;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 module.exports = setup;
